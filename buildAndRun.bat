d5d8e3cc3b966b9989c683c8f70e3dccf2508dc8
paul-gilliard <76849211+paul-gilliard@users.noreply.github.com>
2021-02-02 21:19:46 +0100

    Delete buildAndRun.bat

--- buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t .fr.insset.paulgilliard/premiere_appli_web .
-call docker rm -f premiere_appli_web
-call docker run -d -p 9080:9080 -p 9443:9443 --name premiere_appli_web .fr.insset.paulgilliard/premiere_appli_web
